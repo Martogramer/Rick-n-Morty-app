1d01d1398d032b20d27431d87d19f2f0f89799fd
Marto <contact@torox.com>
2021-12-22 15:59:55 -0300

    character-card

--- rickappmorty/src/components/Characters.js
+++ rickappmorty/src/components/Characters.js
@@ -5,8 +5,14 @@ function Characters({ characters = [] }) {
     <div className="row">
       {characters.map((item, index) => (
         <div key={index} className="col">
-            <div className="card">
+            <div className="card" style={{minWidth: "200px"}}>
                 <img src={item.image} />
+                <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <hr />
+                    <p>specie: {item.species}</p>
+                    <p>location: {item.location.name}</p>
+                </div>
             </div>
         </div>
       ))}
